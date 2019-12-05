ddfd04168aa32771ddca8b7ba5c6fd712688f5ce
Saltibarsciai <rakunas.dominykas@gmail.com>
2019-12-05 02:43:49 +0200

    create apiResource without 3 methods

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,4 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::apiResource('tasks', 'TasksController')->except(['show', 'edit', 'create']);
